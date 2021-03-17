3c61701575d796a157679162a815b426c5538ad5
Osman Can Dülger <59533680+osmancandulger@users.noreply.github.com>
2021-03-17 12:39:09 +0300

    Added CRUD features

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,127 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <h1>Bind</h1>
+    <div id="bind">
+      <input
+        type="text"
+        name="name"
+        id="name"
+        v-model="name"
+        placeholder="Name"
+      />
+      <input
+        type="text"
+        name="lastname"
+        id="lastname"
+        v-model="lastname"
+        placeholder="Last Name"
+      />
+      <input type="button" value="submit" @click="submitItem" />
+      <div class="edit-section">
+        <h1>Edit Name:</h1>
+        <input
+          type="text"
+          name="editName"
+          id="editName"
+          v-model="tempName"
+          placeholder="Edit Name"
+        />
+        <h1>Edit Lastname:</h1>
+        <input
+          type="text"
+          name="editLastname"
+          id="editLastname"
+          v-model="tempLastName"
+          placeholder="Edit Lastname"
+        />
+      </div>
+
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Last Name</th>
+          <th>Actions</th>
+        </tr>
+        <tr v-for="(item, index) in array" :key="index">
+          <td>{{ item.name }}</td>
+          <td>{{ item.lastname }}</td>
+          <td>
+            <input
+              class="edit"
+              type="button"
+              value="Edit"
+              @click="editItem(index)"
+            />
+            <input
+              class="delete"
+              type="button"
+              value="Delete"
+              @click="deleteItem(index)"
+            />
+          </td>
+        </tr>
+      </table>
+    </div>
   </div>
-  <router-view />
 </template>
+<script>
+export default {
+  el: "#app",
+  data() {
+    /* eslint-disable */
+    return {
+      name: "",
+      lastname: "",
+      temp: "",
+      data1: "",
+      tempName: "",
+      tempLastName: "",
+      array: [
+        {
+          name: "Osman Can",
+          lastname: "Dülger",
+        },
+        {
+          name: "Arda",
+          lastname: "Dülger",
+        },
+        {
+          name: "Namık Kemal",
+          lastname: "Dülger",
+        },
+        {
+          name: "Ayşegül",
+          lastname: "Dülger",
+        },
+      ],
+    };
+  },
+  methods: {
+    submitItem() {
+      if (this.name && this.lastname) {
+        this.array.push({ name: this.name, lastname: this.lastname });
+        (this.name = null), (this.lastname = null);
+      } else {
+        alert("Please provide name and lastname !");
+      }
+    },
+    editItem(index) {
+      if (this.tempName && !this.tempLastName) {
+        this.array[index].name = this.tempName;
+      } else if (this.tempLastName && !this.tempName) {
+        this.array[index].lastname = this.tempLastName;
+      } else if (this.tempName && this.tempLastName) {
+        this.array[index].name = this.tempName;
+        this.array[index].lastname = this.tempLastName;
+      }
+      (this.tempName = ""), (this.tempLastName = "");
+    },
+    deleteItem(index) {
+      this.array.splice(index, 1);
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -14,17 +131,35 @@
   text-align: center;
   color: #2c3e50;
 }
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+  margin-top: 35px;
+}
 
-#nav {
-  padding: 30px;
+td,
+th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.edit {
+  background: aliceblue;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 15px;
+  border-radius: 25%;
+}
+.delete {
+  background: rgb(240, 180, 180);
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 10px;
+  border-radius: 25%;
+  margin-left: 15px;
 }
 </style>
---
+++ src/components/First.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  el: "#bind",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="sass" scoped></style>
